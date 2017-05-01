49304c52c0ec440b633975e1fed81db34940070c
SergioR31 <sergio.ramos.c31@gmail.com>
2017-05-01 16:16:17 -0500

    Project Creation

---
+++ Sensor_Temperatura/Sensor_Temperatura.ino
@@ -0,0 +1,60 @@
+int sensorPin = A0;
+int contador = 0;
+
+String entrada;
+
+String address;
+String functionCode;
+String quantity;
+int sizeOfData;
+String data;
+String dataHex = "";
+
+
+void setup(){
+  Serial.begin (9600);
+}
+
+void loop(){
+  int value = analogRead(sensorPin);
+  float millivolts = (value / 1023.0) * 5000;
+  float celsius = millivolts / 10;
+  
+  data = String(celsius);
+  
+  sizeOfData = sizeof(String(celsius));
+  
+  if(Serial.available()){
+
+    entrada = Serial.readString();
+
+    for (int i=0;i<sizeof(sizeOfData);i++){
+      dataHex += String(data.charAt(i),HEX);
+      dataHex += " ";
+    }
+
+     Serial.print(dataHex);
+    
+//
+//    address = entrada.substring(0,2);
+//    functionCode = entrada.substring(3,5);
+//    quantity = entrada.substring(6,8);
+//
+//    switch (functionCode.substring(1,2).toInt()){
+//      
+//      case 1:{
+//        
+//        if (Serial.availableForWrite()){
+//          if(sizeOfData < 10){
+//            Serial.print(functionCode+"/0"+sizeOfData+"/"+dato);
+//          }else{
+//            Serial.print(functionCode+"/"+sizeOfData+"/"+dato);
+//          }
+//        }
+//      }
+//      default:{
+//          Serial.print(functionCode.charAt(2));
+//        }
+//    }
+  }
+}
